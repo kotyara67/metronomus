ba4d24475d4b39c261d5e5c99c6a5e7a986ee672
kotyara <kotkotyara67@gmail.com>
2023-11-28 22:17:34 +0300

    coldfix

--- suka.js
+++ suka.js
@@ -1,18 +1,33 @@
-const sendButton = document.getElementById("bigBlyatButton")
+const sendButton = document.getElementById("bigBlyatButton");
 sendButton.addEventListener("click", calculate);
 
 function calculate(){
+    let summ = 0;
+    let hardsumm = [];
     for (let i = 1; i <= 6; i++) {
-        let col = document.getElementById("col" + i).value
-        let hard = document.getElementById("hard" + i).value
-        let out = document.getElementById("out" + i)
+        let col = document.getElementById("col" + i).value;
+        let hard = document.getElementById("hard" + i).value;
+        let out = document.getElementById("out" + i);
 
-        let summa = 0
-        for(let n = 1; n <= 14; n++){
-            summa += hard;
-        }
+        out.innerText = col * hard;
 
-        out.innerText = col * (0.65 + 0.01 * summa);
+        hardsumm.push(+hard);
+        summ += col * hard;
     }
+
+    let overall = document.getElementById("overall");
+    let efpe = document.getElementById("efpe");
+
+    overall.innerText = "Общее количество: " + summ;
+    efpe.innerText = "FP: " + summ * (0.65 + 0.01 * summa(hardsumm));
+
     return;
+}
+
+function summa(a){
+    let sum = 0;
+    for(let n = 0; n < a.length; n++){
+        sum+= a[n];
+    }
+    return sum;
 }
